bf0eac56289963e4473937b7642b2486c567b151
Trigo YODILA <trigoyodila1996@gmail.com>
2022-06-06 14:09:16 +0200

    visualisation du formulaire

--- js/script.js
+++ js/script.js
@@ -9,3 +9,9 @@ let shoppingCart = document.querySelector(".shopping-cart");
 document.querySelector("#cart-btn").addEventListener("click", () => {
   shoppingCart.classList.toggle("active");
 });
+
+let loginForm = document.querySelector(".login-form");
+
+document.querySelector("#login-btn").addEventListener("click", () => {
+  loginForm.classList.toggle("active");
+});
